1693aac370590c16409ce6dd48dceb3440ed65a3
Helena Martins de Souza <helenamasou@gmail.com>
2019-05-04 10:50:24 -0300

    My first commit

---
+++ hacka/urls.py
@@ -0,0 +1,14 @@
+"""
+Definition of urls for hacka.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+import django.contrib.auth.views
+
+import app.forms
+import app.views
+
+urlpatterns = [
+    url(r'^$', app.views.home, name='home')
+]
